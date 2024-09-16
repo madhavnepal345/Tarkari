cb3368f7f61dc965a32b6bcd6b39e5c9265e0357
madhav <madhavnepal916@gmail.com>
2024-09-16 09:16:13 +0545

    created models

--- tarkari/product/models.py
+++ tarkari/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title=models.CharField(max_length=50)
+    Description=models.TextField(max_length=500)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    category=models.CharField(max_length=30)
+    image=models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
