a1e86232a75f79fab4c8191a27968ca064e753f5
madhav <madhavnepal916@gmail.com>
2024-10-26 17:15:29 +0545

    project

--- tarkari/product/admin.py
+++ tarkari/product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Category
 
-
+admin.site.register(Category)
 admin.site.register(Product)
 
-# Register your models here.
+
--- tarkari/product/models.py
+++ tarkari/product/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
 class Product(models.Model):
     title=models.CharField(max_length=50)
     price=models.DecimalField(max_digits=5, decimal_places=2)
