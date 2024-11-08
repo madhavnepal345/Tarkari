5a52573adff289aab883eabf18411d95d5d509e5
madhav <madhavnepal916@gmail.com>
2024-11-08 20:34:10 +0545

    ....

--- tarkari/myadmin/urls.py
+++ tarkari/myadmin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views  import dashboard
+from . views  import dashboard,ad_product
 
 urlpatterns = [
     path('admi/', dashboard, name='admin'),
+    path('add-product/',ad_product,name='add_product'),
+
 ]
--- tarkari/myadmin/views.py
+++ tarkari/myadmin/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'adminside/index.html')
+
+def ad_product(request):
+    return render(request, 'adminside/add_product.html')
